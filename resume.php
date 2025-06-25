ea4afd76d50f1774747cf5c310cc38744bc73532
Ayush karma <87322014+Ayushk07@users.noreply.github.com>
2025-06-26 02:36:54 +0530

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Resume - Ayush</title>
+    <style>
+      html, body {
+        height: 100%;
+        margin: 0;
+      }
+      iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe src="karma_Ayush.pdf"></iframe>
+  </body>
+</html>
